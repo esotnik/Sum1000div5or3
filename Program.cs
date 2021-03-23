702513c7168820fba3c8a0f1c0e07e980cb992c7
Eve <e_sotnik@bk.ru>
2021-03-23 16:15:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sum1000div5or3
+{
+    // Найти сумму всех положительных чисел меньше 1000 кратных 3 или 5.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
